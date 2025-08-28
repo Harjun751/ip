431155937995de1ed059f27c0c44c78e5587bc5a
Harjun751 <Harjun751@gmail.com>
2025-08-28 13:52:23 +0800

    Added trivial tests

---
+++ src/test/java/marvin/ParserTest.java
@@ -0,0 +1,50 @@
+package marvin;
+
+import marvin.command.AddTaskCommand;
+import marvin.command.Command;
+import marvin.command.MarkTaskCommand;
+import org.junit.jupiter.api.Test;
+
+import static marvin.Parser.parse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ParserTest {
+    @Test
+    public void parse_validMarkInput_returnsMarkCommand(){
+        Command c = parse("mark 1");
+        assertEquals(MarkTaskCommand.class, c.getClass());
+    }
+
+    @Test
+    public void parse_invalidMarkInput_throwsException(){
+        try {
+            Command c = parse("mark abc");
+            fail();
+        } catch (Exception e) {
+            assertEquals(MarvinException.class, e.getClass());
+        }
+    }
+
+    @Test
+    public void parse_validTodoInput_returnsAddTaskCommand(){
+        Command c = parse("todo this");
+        assertEquals(AddTaskCommand.class, c.getClass());
+    }
+
+    @Test
+    public void parse_validDeadlineInput_returnsAddTaskCommand(){
+        Command c = parse("deadline test /by 28/08/2025 1800");
+        assertEquals(AddTaskCommand.class, c.getClass());
+    }
+
+    @Test
+    public void parse_invalidDeadlineInput_throwsException(){
+        try {
+            Command c = parse("deadline test /by 28/8/2025 1800");
+            fail();
+        } catch (Exception e) {
+            assertEquals(MarvinException.class, e.getClass());
+        }
+    }
+}
---
+++ src/test/java/marvin/command/DeleteTaskCommandTest.java
@@ -0,0 +1,41 @@
+package marvin.command;
+
+import marvin.MarvinException;
+import marvin.task.TaskList;
+import marvin.task.Todo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DeleteTaskCommandTest {
+    @Test
+    public void deleteTaskCommand_validCommand_deletesTask(){
+        // Arrange
+        DeleteTaskCommand dtc = new DeleteTaskCommand(0);
+        TaskList tl = new TaskList();
+        tl.addToList(new Todo("Test Todo"));
+
+        // Act
+        dtc.execute(tl);
+
+        // Assert
+        assertEquals("", tl.toString());
+        assertEquals(0, tl.getCount());
+    }
+
+    @Test
+    public void deleteTaskCommand_invalidCommand_doesNotDeleteTask(){
+        // Arrange
+        DeleteTaskCommand dtc = new DeleteTaskCommand(1);
+        TaskList tl = new TaskList();
+        tl.addToList(new Todo("Test Todo"));
+
+        // Act
+        dtc.execute(tl);
+
+        // Assert
+        assertEquals(1, tl.getCount());
+        assertEquals("1. [T][ ] Test Todo\n", tl.toString());
+    }
+}

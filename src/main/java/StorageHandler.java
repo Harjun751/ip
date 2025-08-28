a74f5e01c2bf4e9c533e27893a15b8bfd8e7e218
Harjun751 <Harjun751@gmail.com>
2025-08-28 08:54:21 +0800

    Add save functionality to Marvin
    Data is lost after user closes marvin.
    
    Persisting tasks even after closing is desired
    as users will be able to keep marvin closed
    if they don't need it currently.
    
    Lets
    * Add a storage handler class to handle data files
    * Make tasklists and tasks serializable to save
    * Make marvin load on startup and save after
        every operation

---
+++ src/main/java/StorageHandler.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class StorageHandler {
+    private static final String FILE_PATH = "./save.mrvn";
+
+    public static TaskList loadTaskList() {
+        try {
+            FileInputStream fis = new FileInputStream(FILE_PATH);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            return (TaskList) ois.readObject();
+        } catch (FileNotFoundException e) {
+            return new TaskList();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("(failed to load data - resetting)");
+            return new TaskList();
+        }
+    }
+
+    public static void storeTaskList(TaskList taskList) {
+        try {
+            FileOutputStream fos = new FileOutputStream(FILE_PATH);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(taskList);
+        } catch (IOException e) {
+            System.out.println("(failed to save data - data will not persist.)");
+        }
+    }
+}
